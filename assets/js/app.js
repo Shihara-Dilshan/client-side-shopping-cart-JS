ac2892cc57256812cbbfd8bd07b26c4c10fbf7e8
Shihara-Dilshan <shiharadotadilshanid@gmail.com>
2020-04-08 05:06:55 +0530

    5AM comit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,85 +1,60 @@
 //remove white spaces
 
 function myTrim(x) {
-
-    return x.replace(/\s/g, "");
-
-
+  return x.replace(/\s/g, "");
 }
 
 //show mobile shopping cart
 
 function showMobileCart() {
+  const showCart = document.getElementById("cart");
+  const cartIcon = document.getElementById("cartIcon");
 
-    const showCart = document.getElementById('cart');
-    const cartIcon = document.getElementById('cartIcon');
-
-    showCart.classList.toggle('hideCart');
-    showCart.classList.toggle('showCart');
-
-
-
+  showCart.classList.toggle("hideCart");
+  showCart.classList.toggle("showCart");
 }
-
 
 //show cart
 
 (function () {
+  const showCart = document.getElementById("cart");
+  const cartIcon = document.getElementById("cartIcon");
 
-    const showCart = document.getElementById('cart');
-    const cartIcon = document.getElementById('cartIcon');
-
-    cartIcon.addEventListener("click", function () {
-
-
-
-        showCart.classList.toggle('hideCart');
-        showCart.classList.toggle('showCart');
-
-
-
-    });
-
-
+  cartIcon.addEventListener("click", function () {
+    showCart.classList.toggle("hideCart");
+    showCart.classList.toggle("showCart");
+  });
 })();
-
 
 //add items to the cart
 
 (function () {
+  let totalPrice = 0;
+  let totalItems = 0;
+  const cartBtn = document.querySelectorAll(".card-action");
 
-    let totalPrice = 0;
-    let totalItems = 0;
-    const cartBtn = document.querySelectorAll('.card-action');
+  cartBtn.forEach(function (btn) {
+    btn.addEventListener("click", function (event) {
+      const item = {};
+      let tar = event.target.parentElement.previousElementSibling;
+      let name = tar.children[0].textContent;
+      let price = tar.children[3].textContent;
+      let Oprice = parseInt(price);
+      item.itemName = name;
+      item.itemPrice = Oprice;
 
-    cartBtn.forEach(function (btn) {
+      totalPrice = totalPrice + item.itemPrice;
 
-        btn.addEventListener('click', function (event) {
+      const cartItem = document.createElement("div");
+      cartItem.style.display = "table";
+      cartItem.style.width = "100%";
+      //cartItem.classList.add('showCart', 'test');
 
+      const inputField = myTrim(item.itemName);
 
-            const item = {};
-            let tar = event.target.parentElement.previousElementSibling;
-            let name = tar.children[0].textContent;
-            let price = tar.children[3].textContent;
-            let Oprice = parseInt(price);
-            item.itemName = name;
-            item.itemPrice = Oprice;
+      //console.log(inputField);
 
-            totalPrice = totalPrice + item.itemPrice;
-
-            const cartItem = document.createElement('div');
-            cartItem.style.display = "table";
-            cartItem.style.width = "100%";
-            //cartItem.classList.add('showCart', 'test');
-
-
-            const inputField = myTrim(item.itemName);
-
-
-            //console.log(inputField);
-
-            cartItem.innerHTML =
-                `        
+      cartItem.innerHTML = `        
                 
                <input style="display: table-cell;" id="${inputField}${totalItems}" value="${item.itemName} Rs - ${item.itemPrice}" disabled>  
                       
@@ -91,70 +66,19 @@ function showMobileCart() {
             </div>
 `;
 
+      const tot = document.getElementById("displayPrice");
+      tot.innerHTML = "Total price : " + totalPrice;
 
-
-
-
-            const tot = document.getElementById('displayPrice');
-            tot.innerHTML = "Total price : " + totalPrice;
-
-            const cart = document.getElementById('purchList');
-            cart.appendChild(cartItem);
-            alert('Item Added to the cart');
-            totalItems++;
-
-        });
-
-
-
+      const cart = document.getElementById("purchList");
+      cart.appendChild(cartItem);
+      alert("Item Added to the cart");
+      totalItems++;
     });
-
-
+  });
 })();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 //initialize nevbar
-document.addEventListener('DOMContentLoaded', function () {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems);
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".sidenav");
+  var instances = M.Sidenav.init(elems);
 });
